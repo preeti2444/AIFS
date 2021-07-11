5d55ac2f42af617ab0cf3afaff4d8f5d25b456b3
preeti2444 <87132366+preeti2444@users.noreply.github.com>
2021-07-11 19:48:30 +0530

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,5 @@
+class AppTest{
+	public static void main(String a[]) {
+		System.out.println("App Test");
+	}
+}
---
+++ app.java
@@ -0,0 +1,7 @@
+package com.acc.app;
+
+class AppTest{
+	public static void main(String a[]) {
+		System.out.println("App");
+	}
+}
